a7b47c1a41ed210b3ebf60ea6338076d89eeecd9
SaddamHossain07 <contact.saddam3@gmail.com>
2023-11-29 08:09:01 +0600

    adding authentication

--- index.js
+++ index.js
@@ -45,8 +45,52 @@ async function run() {
             res.send({ token })
         })
 
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded
+                next()
+            })
+        }
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
         // user collection api =================================
-        app.get('/users/:email', async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
+            }
+            res.send({ admin })
+        })
+
+        app.get('/users/:email', verifyToken, async (req, res) => {
             const query = { email: req.params.email }
             const result = await userCollection.findOne(query)
             res.send(result)
@@ -63,6 +107,25 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
         // shop collection ================================
         app.get('/shops/:email', async (req, res) => {
             const query = { ownerEmail: req.params.email }
@@ -70,7 +133,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/shops', async (req, res) => {
+        app.post('/shops', verifyToken, async (req, res) => {
             const shop = req.body;
             const result = await shopCollection.insertOne(shop);
 
@@ -93,22 +156,25 @@ async function run() {
         });
 
         // products collection api ====================================
-        app.get('/products/:email', async (req, res) => {
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/products/:email', verifyToken, async (req, res) => {
             const email = req.params.email
             const query = { shopManager: email }
             const result = await productCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.get('/products/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: new ObjectId(id) }
-            const result = await productCollection.findOne(query)
-            res.send(result)
-        })
-
-
-        app.post('/products', async (req, res) => {
+        // app.post('/products/:id', verifyToken, async (req, res) => {
+        //     const id = req.params.id
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await productCollection.findOne(query)
+        //     res.send(result)
+        // })
+        app.post('/products', verifyToken, async (req, res) => {
             const product = req.body
             const shopId = product.shopId
             const query = { shopId: shopId }
@@ -122,7 +188,7 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/products/:id', async (req, res) => {
+        app.patch('/products/update/:id', verifyToken, async (req, res) => {
             const item = req.body
             const id = req.params.id
             const filter = { _id: new ObjectId(id) }
@@ -143,7 +209,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/products/:id', async (req, res) => {
+        app.delete('/products/:id', verifyToken, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await productCollection.deleteOne(query)
@@ -152,20 +218,20 @@ async function run() {
 
 
         // carts and checkout functionalities ====================
-        app.get('/carts/:email', async (req, res) => {
+        app.get('/carts/:email', verifyToken, async (req, res) => {
             const email = req.params.email
             const query = { shopManager: email }
             const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.post('/carts', async (req, res) => {
+        app.post('/carts', verifyToken, async (req, res) => {
             const item = req.body
             const result = await cartCollection.insertOne(item)
             res.send(result)
         })
 
-        app.delete('/carts/:id', async (req, res) => {
+        app.delete('/carts/:id', verifyToken, async (req, res) => {
             const id = req.params.id
             const query = { _id: id }
             const result = await cartCollection.deleteOne(query)
@@ -173,7 +239,7 @@ async function run() {
         })
 
         // sales collection ==================================
-        app.get('/sales/:email', async (req, res) => {
+        app.get('/sales/:email', verifyToken, async (req, res) => {
             const email = req.params.email
             const query = { shopManager: email }
             const result = await saleCollection.find(query).toArray()
@@ -219,7 +285,7 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/sales', async (req, res) => {
+        app.post('/sales', verifyToken, async (req, res) => {
             const sales = req.body
             const salesResult = await saleCollection.insertOne(sales)
 
